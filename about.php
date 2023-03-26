867afc9f6862f591342ce0fe74c6fa7c10ba33b9
David  Gathura <100764414+wairimu-david@users.noreply.github.com>
2023-03-26 13:30:37 +0300

    Create about.php

---
+++ about.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="shortcut icon" href="image/10.png" type="image/png" >
+
+    <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="cs/bootstrap.min.css">
+
+  <!-- Font Awesome CSS -->
+  <link rel="stylesheet" href="cs/all.min.css">
+
+  <!-- Google Font -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- link to Bootstrap minified css-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- link to Jquery minified-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- link to Bootstrap JS -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <!-- link to external CSS -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta content="text/html; charset=iso-8859-2" http-equiv="Content-Type">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+
+  <style>
+    .bs-example{
+        margin: 1px;
+    }
+
+  </style>
+
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/design.css">
+    <title>About Us</title>
+</head>
+<body  style="background-image: url('image/c.png');">
+    <div class="bs-example">
+    <nav class="navbar navbar-expand-md sticky-top" style="background-color: rgba(0, 0, 0, 0.6);" >
+        <a href="#" class="navbar-brand" style="color: white; font-weight: bold; font-size: 25px;">Online Attendance System</a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse" style="background-color: #F1F0FF; border-radius: 10px; height: 30px;">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <a href="" class="closebtn" onclick="closeNav" style="color:rgba(0, 0, 0, 0);">X</a>
+            
+        </div>
+    </nav>
+
+
+  
+</div>
+
+<div class ="content">
+      <center>
+      <b><div class = "head">ONLINE ATTENDANCE SYSTEM</div></b>
+        <p>
+         Online attendance system ,is a web-based tool for effective management and handling of class attendance in learning institutions. This project is focused on maintaining the student's attendance records that includes but not limited to unit name,student's attendance statistics the details of lecturers and their respective units.The system can be integrated with the already existing systems such as LMS for effective functioning and implementation.</p>  
+      </center>
+    </div>
+    
+
+
+
+</body>
+</html>
